9a13512e0fa3b52bced45092f4c20200b880b7b2
noobdevsam <mdshoykotislam05@gmail.com>
2025-05-25 23:40:34 +0600

    initial commit

---
+++ src/main/java/com/example/springpracticegateway/SpringPracticeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springpracticegateway/SpringPracticeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
